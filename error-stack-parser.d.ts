d9eb56a2804d0e5c1f3bf26e067a09f094fdd0de
Eric Wendelin <me@eriwen.com>
2016-12-09 20:43:50 -0700

    Update TypeScript definitions

--- error-stack-parser.d.ts
+++ error-stack-parser.d.ts
@@ -5,14 +5,52 @@
 
 declare module ErrorStackParser {
     export interface StackFrame {
-        constructor(functionName: string, args: any, fileName: string, lineNumber: number, columnNumber: number, source: string): StackFrame;
+        constructor(object: StackFrame): StackFrame;
+
+        isConstructor?: boolean;
+        getIsConstructor(): boolean;
+        setIsConstructor(): void;
+
+        isEval?: boolean;
+        getIsEval(): boolean;
+        setIsEval(): void;
+
+        isNative?: boolean;
+        getIsNative(): boolean;
+        setIsNative(): void;
+
+        isTopLevel?: boolean;
+        getIsTopLevel(): boolean;
+        setIsTopLevel(): void;
+
+        columnNumber?: number;
+        getColumnNumber(): number;
+        setColumnNumber(): void;
+
+        lineNumber?: number;
+        getLineNumber(): number;
+        setLineNumber(): void;
+
+        fileName?: string;
+        getFileName(): string;
+        setFileName(): void;
 
         functionName?: string;
-        args?: any[];
-        fileName?: string;
-        lineNumber?: number;
-        columnNumber?: number;
+        getFunctionName(): string;
+        setFunctionName(): void;
+
         source?: string;
+        getSource(): string;
+        setSource(): void;
+
+        args?: any[];
+        getArgs(): any[];
+        setArgs(): void;
+
+        evalOrigin?: StackFrame;
+        getEvalOrigin(): StackFrame;
+        setEvalOrigin(): void;
+
         toString(): string;
     }
 
