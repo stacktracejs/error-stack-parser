8f96eab476889230293a71115424e6fa14caca6b
Matt Wynne <matt@cucumber.io>
2021-07-19 14:03:51 -0700

    Add a failling test for node paths with parens

--- spec/fixtures/captured-errors.js
+++ spec/fixtures/captured-errors.js
@@ -397,3 +397,17 @@ CapturedExceptions.NODE_WITH_SPACES = {
     'Function.Module.runMain (internal/modules/cjs/loader.js:837:10)\n    at ' +
     'internal/main/run_main_module.js:17:11'
 };
+
+CapturedExceptions.NODE_WITH_PARENTHESES = {
+    name: 'Error',
+    message: '',
+    stack: 'Error\n    at Object.<anonymous> ' +
+    '(/var/app/scratch/my ' +
+    'project (top secret)/index.js:2:9)\n    at Module._compile ' +
+    '(internal/modules/cjs/loader.js:774:30)\n    at ' +
+    'Object.Module._extensions..js (internal/modules/cjs/loader.js:785:10)\n   ' +
+    ' at Module.load (internal/modules/cjs/loader.js:641:32)\n    at ' +
+    'Function.Module._load (internal/modules/cjs/loader.js:556:12)\n    at ' +
+    'Function.Module.runMain (internal/modules/cjs/loader.js:837:10)\n    at ' +
+    'internal/main/run_main_module.js:17:11'
+};
